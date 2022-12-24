89af029e9e7c163d2624b1c82c7dff724f249abe
carrtesy <tommy.dm.kim@gmail.com>
2022-12-24 18:23:41 +0900

    [20221224] updated USAD script

--- scripts/run_USAD.sh
+++ scripts/run_USAD.sh
@@ -3,9 +3,9 @@ echo "run USAD"
 sh scripts/USAD.sh 0 USAD toyUSW 12 30 4 1 &&
 sh scripts/USAD.sh 0 USAD NeurIPS-TS-UNI 12 30 4 1 &&
 sh scripts/USAD.sh 0 USAD NeurIPS-TS-MUL 12 30 12 1 &&
-#sh scripts/USAD.sh 0 USAD SWaT 12 70  40  5 &&
-#sh scripts/USAD.sh 0 USAD SMD  5  250 38  5 &&
-#sh scripts/USAD.sh 0 USAD SMAP 5  250 55  5 &&
-#sh scripts/USAD.sh 0 USAD MSL  5  250 33  5 &&
-#sh scripts/USAD.sh 0 USAD WADI 10 70  100 5 &&
+sh scripts/USAD.sh 0 USAD SWaT 12 70  40  5 &&
+sh scripts/USAD.sh 0 USAD SMD  5  250 38  5 &&
+sh scripts/USAD.sh 0 USAD SMAP 5  250 55  5 &&
+sh scripts/USAD.sh 0 USAD MSL  5  250 33  5 &&
+sh scripts/USAD.sh 0 USAD WADI 10 70  100 5 &&
 echo "complete"
